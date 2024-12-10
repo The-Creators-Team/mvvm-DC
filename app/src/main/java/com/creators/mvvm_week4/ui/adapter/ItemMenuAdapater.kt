295cc56c131692670e7a31ddf1ae27385dae14f7
DanielCovautl <covault08@gmail.com>
2024-12-10 11:46:27 -0500

    change structure on item_card for LinearLayout and info items

--- app/src/main/java/com/creators/mvvm_week4/ui/adapter/ItemMenuAdapater.kt
+++ app/src/main/java/com/creators/mvvm_week4/ui/adapter/ItemMenuAdapater.kt
@@ -7,24 +7,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.creators.mvvm_week4.R
 import com.creators.mvvm_week4.data.model.ItemMenu
-import com.creators.mvvm_week4.databinding.CocktailCardBinding
+import com.creators.mvvm_week4.databinding.ItemMenuCardBinding
 
 class ItemMenuAdapater(
     val itemMenuList: List<ItemMenu?>?
 ) : RecyclerView.Adapter<ItemMenuAdapater.MyViewHolder>() {
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val binding = CocktailCardBinding.bind(view)
+        private val binding = ItemMenuCardBinding.bind(view)
 
         fun fillInformation(itemMenu: ItemMenu?) {
             binding.apply {
-                tvTitle.text = itemMenu?.nombre
-                tvDescription.text = itemMenu?.descripcion
+                tvname.text = itemMenu?.nombre
+                tvdescripcion.text = itemMenu?.descripcion
+                tvprice.text =   itemMenu?.precio_costo.toString() + " $"
                 Glide.with(itemView)
                     .load( "https://www.we-areunited.com/assets/united-commerce/images/items/" + itemMenu?.image_path)
                     .placeholder(R.drawable.ic_launcher_foreground) //in case of loading or buffering
                     .error(R.drawable.ic_launcher_background) //in case of failure
-                    .into(igCard)
+                    .into(ivImegen)
             }
         }
     }
